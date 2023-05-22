fd690401a3812aba6cc31d1c9b5e87b4985b2367
madhan_uh <110963947+Madhan-19@users.noreply.github.com>
2023-05-22 15:02:48 +0530

    Updated forms db config file

--- form_db_extraction_config/config_client.py
+++ form_db_extraction_config/config_client.py
@@ -1,23 +1,23 @@
 # FORMS database details
-FORMS_DB_SERVER_HOST = ''  # Connection Server
-FORMS_DB_NAME = '' # DatabaseName
-FORMS_DB_USER_NAME = ''  # DBUserName
+FORMS_DB_SERVER_HOST = 'BTG-TRN2016'  # Connection Server
+FORMS_DB_NAME = 'FORMS_STAGE' # DatabaseName
+FORMS_DB_USER_NAME = 'bidev'  # DBUserName
 
 # Azure Key vault name
-KEY_VAULT_NAME = ''  # Azure Keyvault Name
+KEY_VAULT_NAME = 'keyvault-btg-bi-dev'  # Azure Keyvault Name
 
 # Azure keyvault-dev Key vault secrets
-FORMS_KEY_VAULT_SECRET_NAME_DB_PASSWORD = ''  # Azure Keyvault SecretName For DBUserPassword
+FORMS_KEY_VAULT_SECRET_NAME_DB_PASSWORD = 'SourceStage'  # Azure Keyvault SecretName For DBUserPassword
 
 # Max job count
 MAX_JOB_COUNT = 5
 
-AZURE_STORAGE_ACCOUNT_NAME = ""    # Azure Storage Account Name
+AZURE_STORAGE_ACCOUNT_NAME = "storageaccountbtgbidev"    # Azure Storage Account Name
 
-AZURE_STORAGE_ACCOUNTKEY_KEY_VAULT_SECRET_NAME = ""      # Azure Storage AccountKey Keyvault Secret Name 
+AZURE_STORAGE_ACCOUNTKEY_KEY_VAULT_SECRET_NAME = "dlaccesskey"      # Azure Storage AccountKey Keyvault Secret Name 
 
 # Azure container name
-AZURE_STORAGE_ACCOUNT_CONTAINER_NAME = ""       # Azure ContainerName
+AZURE_STORAGE_ACCOUNT_CONTAINER_NAME = "raw"       # Azure ContainerName
 
 # Azure storage account connection string
 AZURE_STORAGE_ACCOUNT_CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName={accountname};AccountKey={accountkey};EndpointSuffix=core.windows.net"    # Azure Storage Account Connection String
